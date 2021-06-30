b83f25ef556501192c6d78fc668def5831a30a3b
Christian daheim <auriocus@gmx.de>
2021-06-30 08:00:33 +0200

    Add version info for different tools

--- make.sh
+++ make.sh
@@ -7,6 +7,12 @@ echo "This is the make script which builds the tarball on $machine"
 mkdir -p build
 echo $machine > build/somefile
 
+for tool in "cmake --version" "gcc --version" "g++ --version" "swig -version"; do 
+	echo "======================================================="
+	echo "Checking for $tool"
+	$tool 
+done
+
 mkdir -p dist
 TARBALL=testball-$machine.tar.bz2
 tar cvjf dist/$TARBALL build
